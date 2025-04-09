8bb93c234745ea3e0efb436900b8f8fc711c211d
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-09 16:46:44 -0400

    feat: add invoice domain

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,8 +3,10 @@ package domain
 import "errors"
 
 var (
-	ErrAccountNotFound = errors.New("account not found")
-	ErrDuplicatedAPIKey = errors.New("api key already exists")
-	ErrInvoiceNotFound = errors.New("invoice not found")
-	ErrUnauthorizedAccess = errors.New("unauthorized access")
+	ErrAccountNotFound = errors.New("account not found") //Erro de conta não encontrada
+	ErrDuplicatedAPIKey = errors.New("api key already exists") //Erro de chave API duplicada
+	ErrInvoiceNotFound = errors.New("invoice not found") //Erro de fatura não encontrada
+	ErrUnauthorizedAccess = errors.New("unauthorized access") //Erro de acesso não autorizado
+	ErrInvalidAmount = errors.New("invalid amount") //Erro de valor inválido
+	ErrInvalidStatus = errors.New("invalid status") //Erro de status inválido
 )
